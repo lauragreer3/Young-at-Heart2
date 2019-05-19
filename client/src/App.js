54d9967208af075e8ac84227eb8cc631c5de5c01
Moonkitten <lauragreer3@gmail.com>
2019-05-18 20:05:54 -0600

    moved components into src file, added login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vacations: []
+    };
+  }
+  componentDidMount() {
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+    axios.get('api/vacation')
+      .then(res => {
+        this.setState({ vacations: res.data });
+        console.log(this.state.vacations);
+      })
+      .catch((error) => {
+        if(error.response.status === 401) {
+          this.props.history.push('/login');
+        }
+      });
+  }
+
+  logout = () => {
+    localStorage.removeItem('jwtToken');
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div>
+        Welcome to Young At Heart
+      </div>
+    )
+  }
+
 }
 
 export default App;
